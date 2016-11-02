6bec22925c7a60a17a38a4f1d2c77e0a448da934
James Kinyua <jaykaynjuguna@gmail.com>
2016-11-02 08:51:12 +0300

    Add logic to find the first n Fibonacci numbers

---
+++ fibonacci.py
@@ -0,0 +1,20 @@
+def fibo(n):
+    if n == 1:
+        return [1]
+    if n == 2:
+        return [1, 1]
+    #n > 2
+    a = 1
+    b = 1
+    #First two members of the series
+    series = [a, b]
+    for each_item in range(n):
+        c = a + b
+        series.append(c)
+        a = b
+        b = c
+
+    return series
+
+if __name__ == '__main__':
+    print(fibo(3))
